247bf08b5bdaabf9dd5cff6b2e31be21894b1970
Alejandro Rosso <deltalejo@gmail.com>
2022-12-11 22:02:49 -0500

    Fix typo.

--- lib/cppcheck.rb
+++ lib/cppcheck.rb
@@ -4,7 +4,7 @@ CPPCHECK_ROOT_NAME = 'cppcheck'.freeze
 CPPCHECK_SYM       = CPPCHECK_ROOT_NAME.to_sym
 CPPCHECK_BUILD_DIR = File.join(PROJECT_BUILD_ROOT, CPPCHECK_ROOT_NAME)
 
-class Cppcheck < plugin
+class Cppcheck < Plugin
   def setup
 	
   end

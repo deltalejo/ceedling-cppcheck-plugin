6657a75cfe03381921c1824d47a27075907f6897
Alejandro Rosso <deltalejo@gmail.com>
2022-12-17 10:11:44 -0500

    Setup tool arguments.

--- lib/cppcheck.rb
+++ lib/cppcheck.rb
@@ -1,11 +1,26 @@
 require 'ceedling/plugin'
 
-CPPCHECK_ROOT_NAME = 'cppcheck'.freeze
-CPPCHECK_SYM       = CPPCHECK_ROOT_NAME.to_sym
-CPPCHECK_BUILD_DIR = File.join(PROJECT_BUILD_ROOT, CPPCHECK_ROOT_NAME)
+CPPCHECK_ROOT_NAME  = 'cppcheck'.freeze
+CPPCHECK_SYM        = CPPCHECK_ROOT_NAME.to_sym
+
+CPPCHECK_BUILD_PATH = File.join(PROJECT_BUILD_ROOT, CPPCHECK_ROOT_NAME)
 
 class Cppcheck < Plugin
   def setup
-	
+    @config = @ceedling[:setupinator].config_hash[CPPCHECK_SYM]
+    @tool = @ceedling[:setupinator].config_hash[:tools][CPPCHECK_SYM]
+    @config[:addons].each do |addon|
+      @tool[:arguments] << "--addon=#{addon}"
+    end
+    @config[:defines].each do |define|
+      @tool[:arguments] << "-D#{define}"
+    end
+    @config[:undefines].each do |undefine|
+      @tool[:arguments] << "-U#{undefine}"
+    end
+    @config[:options].each do |option|
+      @tool[:arguments] << option
+    end
+    @tool[:arguments] << '${1}'
   end
 end

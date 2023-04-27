7424da5c64180dd8cba409785dac372902b14f0a
Alejandro Rosso <deltalejo@gmail.com>
2023-04-27 00:56:42 -0500

    Support more Cppcheck options.

--- lib/cppcheck.rb
+++ lib/cppcheck.rb
@@ -1,16 +1,17 @@
 require 'ceedling/plugin'
 require 'cppcheck_defaults'
 
-CPPCHECK_ROOT_NAME          = 'cppcheck'.freeze
-CPPCHECK_TASK_ROOT          = CPPCHECK_ROOT_NAME + ':'
-CPPCHECK_SYM                = CPPCHECK_ROOT_NAME.to_sym
-CPPCHECK_HTMLREPORT_SYM     = CPPCHECK_ROOT_NAME + '_htmlreport'
+CPPCHECK_ROOT_NAME           = 'cppcheck'.freeze
+CPPCHECK_TASK_ROOT           = CPPCHECK_ROOT_NAME + ':'
+CPPCHECK_SYM                 = CPPCHECK_ROOT_NAME.to_sym
+CPPCHECK_HTMLREPORT_SYM      = (CPPCHECK_ROOT_NAME + '_htmlreport').to_sym
 
-CPPCHECK_BUILD_PATH         = File.join(PROJECT_BUILD_ROOT, CPPCHECK_ROOT_NAME)
-CPPCHECK_ARTIFACTS_PATH     = File.join(PROJECT_BUILD_ARTIFACTS_ROOT, CPPCHECK_ROOT_NAME)
+CPPCHECK_BUILD_PATH          = File.join(PROJECT_BUILD_ROOT, CPPCHECK_ROOT_NAME)
+CPPCHECK_ARTIFACTS_PATH      = File.join(PROJECT_BUILD_ARTIFACTS_ROOT, CPPCHECK_ROOT_NAME)
 
-CPPCHECK_ARTIFACTS_FILE     = File.join(CPPCHECK_ARTIFACTS_PATH, 'CppcheckResults.txt')
-CPPCHECK_ARTIFACTS_FILE_XML = File.join(CPPCHECK_ARTIFACTS_PATH, File.basename(CPPCHECK_ARTIFACTS_FILE, '.*') + '.xml')
+CPPCHECK_ARTIFACTS_FILE_TEXT = File.join(CPPCHECK_ARTIFACTS_PATH, 'CppcheckResults.txt')
+CPPCHECK_ARTIFACTS_FILE_XML  = File.join(CPPCHECK_ARTIFACTS_PATH, File.basename(CPPCHECK_ARTIFACTS_FILE_TEXT, '.*') + '.xml')
+CPPCHECK_ARTIFACTS_FILE_HTML = File.join(CPPCHECK_ARTIFACTS_PATH, File.basename(CPPCHECK_ARTIFACTS_FILE_TEXT, '.*') + '.html')
 
 class Cppcheck < Plugin
   def setup
@@ -24,64 +25,80 @@ class Cppcheck < Plugin
     @ceedling[:configurator_builder].populate_defaults(project_config, cppcheck_defaults)
     
     @config = project_config[CPPCHECK_SYM]
-    @tool = project_config[:tools][CPPCHECK_SYM]
-    @htmlreport_tool = project_config[:tools][CPPCHECK_HTMLREPORT_SYM]
+    @cppcheck = project_config[:tools][CPPCHECK_SYM]
+    @cppcheck_htmlreport = project_config[:tools][CPPCHECK_HTMLREPORT_SYM]
     
-    if @config[:html_report] && (!@config[:file_report] || !@config[:xml_report])
-      raise 'File and XML reports must be enabled when asking for HTML report.'
+    @config[:text_artifact_filename] ||= CPPCHECK_ARTIFACTS_FILE_TEXT
+    @config[:xml_artifact_filename] ||= CPPCHECK_ARTIFACTS_FILE_XML
+    @config[:html_artifact_filename] ||= CPPCHECK_ARTIFACTS_FILE_HTML
+    
+    unless @config[:platform].nil? || @config[:platform].empty?
+      @cppcheck[:arguments] << "--platform=#{@config[:platform]}"
     end
     
-    @tool[:arguments] << '--xml' if @config[:xml_report]
-    
-    if @config[:file_report]
-      if @config[:xml_report]
-        @output_file = @config[:xml][:artifact_filename] || CPPCHECK_ARTIFACTS_FILE_XML
-      else
-        @output_file = @config[:text][:artifact_filename] || CPPCHECK_ARTIFACTS_FILE
-      end
-      
-      @tool[:arguments] << "--output-file=#{@output_file}"
+    unless @config[:template].nil? || @config[:template].empty?
+      @cppcheck[:arguments] << "--template=#{@config[:template]}"
     end
     
-    @config[:addons].each do |addon|
-      @tool[:arguments] << "--addon=#{addon}"
+    unless @config[:standard].nil? || @config[:standard].empty?
+      @cppcheck[:arguments] << "--std=#{@config[:standard]}"
     end
     
-    @config[:defines].each do |define|
-      @tool[:arguments] << "-D#{define}"
+    @cppcheck[:arguments] << "--inline-suppr" if @config[:inline_suppressions] == true
+    
+    unless @config[:enable_checks].nil? || @config[:enable_checks].empty?
+      @cppcheck[:arguments] << "--enable=#{@config[:enable_checks].join(',')}"
     end
     
-    @config[:undefines].each do |undefine|
-      @tool[:arguments] << "-U#{undefine}"
+    unless @config[:disable_checks].nil? || @config[:disable_checks].empty?
+      @cppcheck[:arguments] << "--disable=#{@config[:disable_checks].join(',')}"
     end
     
-    @config[:options].each do |option|
-      @tool[:arguments] << option
+    @config[:addons]&.each do |addon|
+      @cppcheck[:arguments] << "--addon=#{addon}"
     end
     
-    @tool[:arguments] << '${1}'
+    @config[:includes]&.each do |include|
+      @cppcheck[:arguments] << "--include=#{include}"
+    end
     
-    config = {
-      :tools => {
-        :cppcheck => @tool,
-        :cppcheck_htmlreport => @htmlreport_tool
-      }
+    @config[:libraries]&.each do |library|
+      @cppcheck[:arguments] << "--library=#{library}"
+    end
+    
+    @config[:rules]&.each do |rule|
+      @cppcheck[:arguments] << "--rule=#{rule}"
+    end
+    
+    @config[:suppressions]&.each do |suppression|
+      @cppcheck[:arguments] << "--suppress=#{suppression}"
+    end
+    
+    @config[:defines]&.each do |define|
+      @cppcheck[:arguments] << "-D#{define}"
+    end
+    
+    @config[:undefines]&.each do |undefine|
+      @cppcheck[:arguments] << "-U#{undefine}"
+    end
+    
+    @config[:options]&.each do |option|
+      @cppcheck[:arguments] << option
+    end
+    
+    @cppcheck[:arguments] << '${1}'
+    
+    @cppcheck_htmlreport[:arguments] << "--file=#{@config[:xml_artifact_filename]}"
+    @cppcheck_htmlreport[:arguments] << "--report-dir=#{File.join(CPPCHECK_ARTIFACTS_PATH, 'html')}"
+    @cppcheck_htmlreport[:arguments] << "--source-dir=#{PROJECT_ROOT}"
+    unless @config[:html_title].nil? || @config[:html_title].empty?
+      @cppcheck_htmlreport[:arguments] << "--title=#{@config[:html_title]}"
+    end
+    
+    tools = {
+      :cppcheck => @cppcheck,
+      :cppcheck_htmlreport => @cppcheck_htmlreport
     }
-    @ceedling[:configurator].build_supplement(project_config, config)
-  end
-  
-  def config
-    return @config
-  end
-  
-  def html_report_options
-    options = [
-      "--file=#{@output_file}",
-      "--report-dir=#{File.join(CPPCHECK_ARTIFACTS_PATH, 'html')}",
-      "--source-dir=#{PROJECT_ROOT}"
-    ]
-    options << "--title=#{@config[:html][:title]}" unless @config[:html][:title].nil?
-    
-    return options
+    @ceedling[:configurator].build_supplement(project_config, {:cppcheck => @config, :tools => tools})
   end
 end

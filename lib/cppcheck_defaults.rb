eae0937f977b001ccb45598d2dbeb2515ddf1a11
Alejandro Rosso <deltalejo@gmail.com>
2024-01-09 22:05:34 -0500

    Remove deprecated background task execution tool option.

--- lib/cppcheck_defaults.rb
+++ lib/cppcheck_defaults.rb
@@ -2,7 +2,6 @@ DEFAULT_CPPCHECK_TOOL = {
   :executable => (ENV['CPPCHECK'].nil? ? FilePathUtils.os_executable_ext('cppcheck') : ENV['CPPCHECK'].split[0]).freeze,
   :name => 'default_cppcheck'.freeze,
   :stderr_redirect => StdErrRedirect::AUTO.freeze,
-  :background_exec => BackgroundExec::NONE.freeze,
   :optional => false.freeze,
   :arguments => [
     '--cppcheck-build-dir=#{CPPCHECK_BUILD_PATH}'.freeze,
@@ -14,7 +13,6 @@ DEFAULT_CPPCHECK_HTMLREPORT_TOOL = {
   :executable => (ENV['CPPCHECK_HTMLREPORT'].nil? ? FilePathUtils.os_executable_ext('cppcheck-htmlreport') : ENV['CPPCHECK_HTMLREPORT'].split[0]).freeze,
   :name => 'default_cppcheck_htmlreport'.freeze,
   :stderr_redirect => StdErrRedirect::NONE.freeze,
-  :background_exec => BackgroundExec::NONE.freeze,
   :optional => false.freeze,
   :arguments => [].freeze
 }

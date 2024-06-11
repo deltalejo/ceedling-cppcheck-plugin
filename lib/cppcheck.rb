01adc5ee2510199bf391a8a489abdb4a9120d5bc
Alejandro Rosso <deltalejo@gmail.com>
2024-06-10 20:31:59 -0500

    Big refactor.

--- lib/cppcheck.rb
+++ lib/cppcheck.rb
@@ -1,126 +1,74 @@
 require 'ceedling/plugin'
-
-CPPCHECK_ROOT_NAME           = 'cppcheck'.freeze
-CPPCHECK_TASK_ROOT           = CPPCHECK_ROOT_NAME + ':'
-CPPCHECK_SYM                 = CPPCHECK_ROOT_NAME.to_sym
-CPPCHECK_HTMLREPORT_SYM      = (CPPCHECK_ROOT_NAME + '_htmlreport').to_sym
-
-CPPCHECK_BUILD_PATH          = File.join(PROJECT_BUILD_ROOT, CPPCHECK_ROOT_NAME)
-CPPCHECK_ARTIFACTS_PATH      = File.join(PROJECT_BUILD_ARTIFACTS_ROOT, CPPCHECK_ROOT_NAME)
-CPPCHECK_ARTIFACTS_HTML_PATH = File.join(CPPCHECK_ARTIFACTS_PATH, 'html')
-
-CPPCHECK_ARTIFACTS_FILE_TEXT = 'CppcheckReport.txt'
-CPPCHECK_ARTIFACTS_FILE_XML  = File.basename(CPPCHECK_ARTIFACTS_FILE_TEXT).ext('.xml')
+require 'cppcheck_constants'
 
 class Cppcheck < Plugin
-  attr_reader :config
-  
   def setup
-    project_config = @ceedling[:setupinator].config_hash
+    @config = @ceedling[:setupinator].config_hash[CPPCHECK_SYM]
+    
+    validate_enabled_reports()
+    
+    if @config[:reports].include?(ReportTypes::HTML)
+      @ceedling[:tool_validator].validate(
+        tool: TOOLS_CPPCHECK_HTMLREPORT,
+        boom: true
+      )
+    end
     
     @ceedling[:configurator].replace_flattened_config(
       collect_suppressions(@ceedling[:configurator].project_config_hash)
     )
     
-    @config = project_config[CPPCHECK_SYM]
-    @cppcheck = project_config[:tools][CPPCHECK_SYM]
-    @cppcheck_htmlreport = project_config[:tools][CPPCHECK_HTMLREPORT_SYM]
+    @text_artifact_filepath = form_text_artifact_filepath(
+      @config[:text_artifact_filename] || CPPCHECK_ARTIFACTS_FILE_TEXT
+    )
     
-    @config[:reports] = ['text'] if @config[:reports].nil? || @config[:reports].empty?
-    @config[:text_artifact_filename] ||= CPPCHECK_ARTIFACTS_FILE_TEXT
-    @config[:xml_artifact_filename] ||= CPPCHECK_ARTIFACTS_FILE_XML
-    
-    unless @config[:platform].nil? || @config[:platform].empty?
-      @cppcheck[:arguments] << "--platform=#{@config[:platform]}"
-    end
-    
-    unless @config[:template].nil? || @config[:template].empty?
-      @cppcheck[:arguments] << "--template=#{@config[:template]}"
-    end
-    
-    unless @config[:standard].nil? || @config[:standard].empty?
-      @cppcheck[:arguments] << "--std=#{@config[:standard]}"
-    end
-    
-    @cppcheck[:arguments] << "--inline-suppr" if @config[:inline_suppressions] == true
-    
-    unless @config[:disable_checks].nil? || @config[:disable_checks].empty?
-      @cppcheck[:arguments] << "--disable=#{@config[:disable_checks].join(',')}"
-    end
-    
-    @config[:addons]&.each do |addon|
-      @cppcheck[:arguments] << "--addon=#{addon}"
-    end
-    
-    @config[:includes]&.each do |include|
-      @cppcheck[:arguments] << "--include=#{include}"
-    end
-    
-    @config[:excludes]&.each do |exclude|
-      @cppcheck[:arguments] << "-i#{exclude}"
-    end
-    
-    @config[:libraries]&.each do |library|
-      @cppcheck[:arguments] << "--library=#{library}"
-    end
-    
-    @config[:rules]&.each do |rule|
-      @cppcheck[:arguments] << "--rule=#{rule}"
-    end
-    
-    COLLECTION_ALL_CPPCHECK.each do |suppression|
-      option = suppression.end_with?('.xml')? '--suppress-xml' : '--suppressions-list'
-      @cppcheck[:arguments] << "#{option}=#{suppression}"
-    end
-    
-    @config[:suppressions]&.each do |suppression|
-      @cppcheck[:arguments] << "--suppress=#{suppression}"
-    end
-    
-    @config[:defines]&.each do |define|
-      @cppcheck[:arguments] << "-D#{define}"
-    end
-    
-    @config[:undefines]&.each do |undefine|
-      @cppcheck[:arguments] << "-U#{undefine}"
-    end
-    
-    @config[:options]&.each do |option|
-      @cppcheck[:arguments] << option
-    end
-    
-    @cppcheck[:arguments] << '${1}'
-    
-    xml_artifact_filename = form_xml_artifact_filepath(@config[:xml_artifact_filename])
-    
-    @cppcheck_htmlreport[:arguments] << "--file=#{xml_artifact_filename}"
-    @cppcheck_htmlreport[:arguments] << "--report-dir=#{CPPCHECK_ARTIFACTS_HTML_PATH}"
-    @cppcheck_htmlreport[:arguments] << "--source-dir=."
-    unless @config[:html_title].nil? || @config[:html_title].empty?
-      @cppcheck_htmlreport[:arguments] << "--title=#{@config[:html_title]}"
-    end
-    
-    @ceedling[:configurator].build_supplement(
-      project_config,
-      {
-        :cppcheck => @config,
-        :tools => {
-          :cppcheck => @cppcheck,
-          :cppcheck_htmlreport => @cppcheck_htmlreport
-        }
-      }
+    @xml_artifact_filepath = form_xml_artifact_filepath(
+      @config[:xml_artifact_filename] || CPPCHECK_ARTIFACTS_FILE_XML
     )
   end
   
-  def form_text_artifact_filepath(filename)
-    return File.join(CPPCHECK_ARTIFACTS_PATH, File.basename(filename).ext('.txt'))
+  def generate_reports()
+    args_common = args_builder_common()
+    args_common << "--enable=all"
+    
+    if @config[:reports].include?(ReportTypes::TEXT)
+      generate_text_report(args_common)
+    end
+    
+    if @config[:reports].include?(ReportTypes::XML)
+      generate_xml_report(args_common)
+    end
+    
+    if @config[:reports].include?(ReportTypes::HTML)
+      generate_html_report(args_common)
+    end
   end
   
-  def form_xml_artifact_filepath(filename)
-    return File.join(CPPCHECK_ARTIFACTS_PATH, File.basename(filename).ext('.xml'))
+  def analyze_file(filepath)
+    args = args_builder_common()
+    
+    unless @config[:enable_checks].nil? || @config[:enable_checks].empty?
+      args << "--enable=#{@config[:enable_checks].join(',')}"
+    end
+    
+    results = run(args, filepath, COLLECTION_PATHS_INCLUDE)
+    @ceedling[:loginator].log(results[:output], Verbosity::COMPLAIN, LogLabels::NONE)
   end
   
   private
+  
+  def validate_enabled_reports(boom:false)
+    all_valid = @config[:reports].all? do |report|
+      valid = ReportTypes::is_supported?(report)
+      @ceedling[:loginator].log(
+        "Report '#{report}' is not supported.",
+        Verbosity::ERRORS
+      ) unless valid
+      valid
+    end
+    
+    raise CeedlingException.new("Not supported reports have been requested.") if boom and !all_valid
+  end
   
   def collect_suppressions(in_hash)
     all_suppressions = @ceedling[:file_wrapper].instantiate_file_list
@@ -139,6 +87,144 @@ class Cppcheck < Plugin
     return {
       :collection_all_cppcheck => all_suppressions
     }
+  end
+  
+  def form_text_artifact_filepath(filename)
+    return File.join(CPPCHECK_ARTIFACTS_PATH, File.basename(filename).ext('.txt'))
+  end
+  
+  def form_xml_artifact_filepath(filename)
+    return File.join(CPPCHECK_ARTIFACTS_PATH, File.basename(filename).ext('.xml'))
+  end
+  
+  def args_builder_common()
+    args = []
+    
+    unless @config[:platform].nil? || @config[:platform].empty?
+      args << "--platform=#{@config[:platform]}"
+    end
+    
+    unless @config[:template].nil? || @config[:template].empty?
+      args << "--template=#{@config[:template]}"
+    end
+    
+    unless @config[:standard].nil? || @config[:standard].empty?
+      args << "--std=#{@config[:standard]}"
+    end
+    
+    args << "--inline-suppr" if @config[:inline_suppressions] == true
+    
+    unless @config[:check_level].nil? || @config[:check_level].empty?
+      args << "--check-level=#{check_level}"
+    end
+    
+    unless @config[:disable_checks].nil? || @config[:disable_checks].empty?
+      args << "--disable=#{@config[:disable_checks].join(',')}"
+    end
+    
+    @config[:addons]&.each do |addon|
+      args << "--addon=#{addon}"
+    end
+    
+    @config[:includes]&.each do |include|
+      args << "--include=#{include}"
+    end
+    
+    @config[:excludes]&.each do |exclude|
+      args << "-i#{exclude}"
+    end
+    
+    @config[:libraries]&.each do |library|
+      args << "--library=#{library}"
+    end
+    
+    @config[:rules]&.each do |rule|
+      args << "--rule=#{rule}"
+    end
+    
+    COLLECTION_ALL_CPPCHECK.each do |suppression|
+      option = suppression.end_with?('.xml')? '--suppress-xml' : '--suppressions-list'
+      args << "#{option}=#{suppression}"
+    end
+    
+    @config[:suppressions]&.each do |suppression|
+      args << "--suppress=#{suppression}"
+    end
+    
+    @config[:defines]&.each do |define|
+      args << "-D#{define}"
+    end
+    
+    @config[:undefines]&.each do |undefine|
+      args << "-U#{undefine}"
+    end
+    
+    args += @config[:arguments]
+  end
+  
+  def args_builder_text()
+    return ["--output-file=#{@text_artifact_filepath}"]
+  end
+  
+  def args_builder_xml()
+    return ["--xml", "--output-file=#{@xml_artifact_filepath}"]
+  end
+  
+  def args_builder_html()
+    args = []
+    
+    args << "--file=#{@xml_artifact_filepath}"
+    args << "--report-dir=#{CPPCHECK_ARTIFACTS_HTML_PATH}"
+    args << "--source-dir=."
+    
+    unless @config[:html_title].nil? || @config[:html_title].empty?
+      args << "--title=#{@config[:html_title]}"
+    end
+    
+    return args
+  end
+  
+  def run(args, *params)
+    command = @ceedling[:tool_executor].build_command_line(
+      TOOLS_CPPCHECK,
+      args,
+      *params
+    )
+    @ceedling[:loginator].log("Command: #{command}", Verbosity::DEBUG)
+    
+    results = @ceedling[:tool_executor].exec(command)
+    
+    return results
+  end
+  
+  def generate_text_report(args_common)
+    args = args_common
+    args += args_builder_text()
+    
+    @ceedling[:loginator].log("Creating Cppcheck text report...", Verbosity::NORMAL)
+    results = run(args, COLLECTION_PATHS_SOURCE, COLLECTION_PATHS_INCLUDE)
+  end
+  
+  def generate_xml_report(args_common)
+    args = args_common
+    args += args_builder_xml
+    
+    @ceedling[:loginator].log("Creating Cppcheck xml report...", Verbosity::NORMAL)
+    results = run(args, COLLECTION_PATHS_SOURCE, COLLECTION_PATHS_INCLUDE)
+  end
+  
+  def generate_html_report(args_common)
+    generate_xml_report(args_common) unless @ceedling[:file_wrapper].exist?(@xml_artifact_filepath)
+    
+    @ceedling[:loginator].log("Creating Cppcheck html report...", Verbosity::NORMAL)
+    
+    command = @ceedling[:tool_executor].build_command_line(
+      TOOLS_CPPCHECK_HTMLREPORT,
+      args_builder_html()
+    )
+    @ceedling[:loginator].log("Command: #{command}", Verbosity::DEBUG)
+    
+    @ceedling[:tool_executor].exec(command)
   end
 end
 
